7c1f789e0b96679089bdfa6d1cbce7e90ebe992c
AndySrb <andrejagrujic2001@gmail.com>
2019-12-02 12:58:11 +0100

    FontMenager added: Test Later

--- src/Renderer/FontMenager.h
+++ src/Renderer/FontMenager.h
@@ -1,4 +1,29 @@
 #ifndef FONTMENAGER_H
 #define FONTMENAGER_H 
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <vector>
+
+#warning Add documetation
+#warning Do test
+
+class FontMenager
+{
+
+	private:
+		FontMenager(const FontMenager &obj);
+		FontMenager(FontMenager &&obj);
+		
+		static FontMenager *s_FontMenager;
+		std::vector<sf::Font> m_fonts;
+
+	public:
+		FontMenager();
+		FontMenager(sf::Font);
+		sf::Font getFont(const int &number);
+		void addFont(const std::string &filename);
+		
+
+};
 
 #endif
